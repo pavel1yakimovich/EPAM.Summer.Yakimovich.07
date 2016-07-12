71b72c2554191404d19fb564d4794dca78f1376e
pavel1yakimovich <pavel1yakimovich@gmail.com>
2016-07-12 20:13:00 +0300

    'commit'

---
+++ Task02Logic/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02Logic
+{
+    public class Book : IEquatable<Book>, IComparable<Book>
+    {
+        public string Author { get; set; }
+
+        public string Title { get; set; }
+
+        public int Pages { get; set; }
+
+        public int Pictures { get; set; }
+
+        public int YearOfPublishing { get; set; }
+
+        public int CompareTo(Book other)
+        {
+            return this.Pages.CompareTo(other.Pages);
+        }
+
+        public bool Equals(Book other)
+        {
+            return (this.Author == other.Author && this.Title == other.Title);
+        }
+    }
+}
